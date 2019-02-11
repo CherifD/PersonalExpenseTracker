e1777ddc0db927e2893ea169590bd25c5265a303
Cherif Diallo <cherifdmc@gmail.com>
2019-02-10 23:25:30 -0500

    feat: Begin Task 3 implementation (layout)

--- app/src/main/java/com/cherifcodes/personalexpensetracker/MainActivity.java
+++ app/src/main/java/com/cherifcodes/personalexpensetracker/MainActivity.java
@@ -1,13 +1,21 @@
 package com.cherifcodes.personalexpensetracker;
 
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PieSummary.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
